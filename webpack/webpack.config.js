5ac3cc4381bd247a806e14b85015cb1c9d8cf9e2
Yordy Santos <syordya@gmail.com>
2020-10-21 13:03:33 -0500

    config

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebPackPlugin = require('html-webpack-plugin'),
   MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: './index.js',
+  entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'load'),
